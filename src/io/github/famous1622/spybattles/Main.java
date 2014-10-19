d6f89e66c83a2845b59f35fcaf72ecef047a2cef
famous1622 <kboorocks@gmx.com>
2014-10-19 15:22:41 -0400

    Adding model and Rendering it

--- src/io/github/famous1622/spybattles/Main.java
+++ src/io/github/famous1622/spybattles/Main.java
@@ -1,11 +1,18 @@
 package io.github.famous1622.spybattles;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
 public class Main {
 	public void start() {
+		Path path = Paths.get("ak47.obj");
 		try {
 			Display.setDisplayMode(new DisplayMode(800,600));
 			Display.create();
@@ -14,8 +21,14 @@ public class Main {
 			System.exit(0);
 		}
 		
-		// init OpenGL here
-		
+		 FileReader fr = null;
+		try {
+			fr = new FileReader(path.toString());
+		} catch (FileNotFoundException e) {
+			System.out.println("failed to load model");
+		}
+		 BufferedReader br = new BufferedReader(fr);
+		 new Object3D(br,true);
 		while (!Display.isCloseRequested()) {
 			
 			// render OpenGL here
